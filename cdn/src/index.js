7f9859b06910d314e99e08793d81e096ef1bcbd6
r2gul4r <leflex0821@gmail.com>
2025-11-25 12:35:47 +0900

    11

---
+++ cdn/src/index.js
@@ -0,0 +1,6 @@
+function App2() {
+  return<h1>hello4</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+      root.render(<App2 />);
---
+++ cra/my-app/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+function App2() {
+  return<h1>hello cra</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+      root.render(<App2 />);
+
