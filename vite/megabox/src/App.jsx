72135e1cc0885a1e6357767195ed766c78f9de56
r2gul4r <leflex0821@gmail.com>
2025-12-04 09:09:18 +0900

    12

--- vite/megabox/src/App.jsx
+++ vite/megabox/src/App.jsx
@@ -1,9 +1,11 @@
 import BoxOfficeCard from './BoxOfficeCard.jsx';
+import {datas} from './data.js';
 
 function App() {
+    // Each child in a list should have a unique "key" prop
     return (
         <div className="box_office">
-            <BoxOfficeCard />
+            {datas.map((ele)=>{return <BoxOfficeCard key={ele.rank} data={ele} />})}
         </div>
     );
 }
--- vite/megabox/src/BoxOfficeCard.jsx
+++ vite/megabox/src/BoxOfficeCard.jsx
@@ -1,38 +1,39 @@
 import "./css/reset.css";
 import "./css/BoxOfficeCard.css";
-import poster from "./images/YiSbqEf6OvFcDoLoQCipDojOHqMCwKG4_420.jpg";
-import mx4d from "./images/type_mega_mx4d.png";
-import cinema from "./images/type_dolbycinema.png";
-import atmos from "./images/type_dolbyatmos.png";
-import ageAll from "./images/ALL_56x56.png";
+// public 폴더에 이미지가 들어 있을 때 ('/'로 시작)
+// import poster from "/images/YiSbqEf6OvFcDoLoQCipDojOHqMCwKG4_420.jpg";
+// src 폴더에 이미지가 들어 있을 때 ('./로 시작)
+// import mx4d from "./images/type_mega_mx4d.png";
 
-        function BoxOfficeCard() {
+        function BoxOfficeCard({data}) {
 
             return (
                 <div className="box_office_card">
                     <div className="inner">
                         <div className="front">
-                            <span className="rank">1</span>
+                            <span className="rank">{data.rank}</span>
                             <div className="poster">
-                                <img src={poster} alt="쥬토피아" />
+                                <img src={data.poster} alt={data.title} />
                             </div>
                             <div className="screen_type">
-                                <img src={mx4d} alt="Mega MX4D" />
-                                <img src={cinema} alt="Dolby Cinema" />
-                                <img src={atmos} alt="Dolby Atmos" />
+                                {/* {data.screenTypes.map((ele, idx)=><img src={data.screenTypes[idx]} alt="screen types" />)} */}
+                                {data.screenTypes.map(ele=><img key={ele} src={ele} alt="screen types" />)}
+                                {/* <img src={data.screenTypes[0]} alt="Mega MX4D" />
+                                <img src={data.screenTypes[1]} alt="Dolby Cinema" />
+                                <img src={data.screenTypes[2]} alt="Dolby Atmos" /> */}
                             </div>
                             <div className="movie_grade">
-                                <img src={ageAll} alt="Movie Grade All" />
+                                <img src={data.grade} alt={"Movie Grade" + data.age} />
                             </div>
                         </div>
                         <div className="back">
-                            <p>더 화려해진 세계, 더 넓어진 주토피아!<br />디즈니의 가~~장 사랑스러운 콤비 '주디'와 '닉'이 돌아온다!<br /><br />미스터리한 뱀 '게리'가 나타난 순간.</p>
-                            <div className="score">관람평<span>9.3</span></div>
+                            <p>{data.description}</p>
+                            <div className="score">관람평<span>{data.score}</span></div>
                         </div>
                     </div>
                     <div className="btn">
                         <button type="button" className="like">
-                            <i className="fa-regular fa-heart"></i>3.6k
+                            <i className="fa-regular fa-heart"></i>{data.like}
                         </button>
                         <a href="https://www.megabox.co.kr/booking" className="reservation_link">예매</a>
                     </div>
