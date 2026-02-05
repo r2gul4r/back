27c0ceae29b3a45593b1841f78eb31ced494c96a
hoho <leflex0821@gmail.com>
2026-02-05 15:38:07 +0900

    0205

---
+++ jsp/my-jsp/src/main/java/controller/Helloservlet.java
@@ -0,0 +1,29 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 
+ * http://localhost:8080/프로젝트명/URL맵핑
+ * 
+ */
+
+@WebServlet("/hello")
+public class Helloservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
